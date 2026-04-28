166a580200f1adc5c9d2233f070521fe1e890cf0
Kiefer Villarreiz <kvillarr@nyit.edu>
2026-04-28 18:51:36 -0400

    routes

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,15 @@ router.get('/', (req,res) =>{
 router.get('/new', (req,res) =>{  // /user/new
     res.send('User New Form')
 })
+router.route('/id').get((req,res)=>{
+    res.send(`Getting User data for id: ${req.params.id} `);
+}).delete((req,res)=>{
+    res.send(`Deleting User data for id: ${req.params.id}`);
+}).put((req,res)=>{
+    res.send(`Updating User data for id: ${req.params.id}`);
+});
 
+//router.get('/:id', (req,res)=>{
+//res.send(`Getting User Data: ${req.params.id}`);
+//});
 module.exports = router;
