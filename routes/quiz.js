d84fa738487cadc80807767a82669bba2dad545b
Kiefer Villarreiz <kvillarr@nyit.edu>
2026-04-28 23:50:53 -0400

    Generated Chosen words array and sent to ejs

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,27 +1,49 @@
 const express = require('express');
+const { readFile } = require('fs').promises;
 const router = express.Router();
-const{readFile} = require('fs').promises;
 
-router.get("/",(req,res)=>{
 
+router.get("/",async(req,res)=>{
+ let chosenWords = await getWords();
+ console.log("Chosen Words: ", chosenWords);
 });
 
 let getWords = async() =>{
+    console.log("Getting random Part!")
 let randomPart = getRandomPart();
+console.log("Random part: ", randomPart)
+let allWords = await readFile('resources/allwords.txt', 'utf8');
+let wordArray = allWords.split('\n');
+shuffle(wordArray);
+
+let choices = [];
+while(choices.length < 5) {
+    let line = wordArray.pop();
+    let [word,part,def] = line.split('\t');
+    //let tokens = line.split('\t');
+   // let word = tokens[0];
+    //let part = tokens[1];
+    //let def = tokens[2];
+    if(part === randomPart){
+        choices.push(line);
+    }
+}
+return choices;
 }
 let getRandomPart = ()=>{
     let parts = ['noun','verb','adjective'];
-    let randomIndex = (Math.random()*parts.length);
-    let randomPart = part[randomIndex];
+    let randomIndex = Math.floor(Math.random()*parts.length);
+    let randomPart = parts[randomIndex];
     return randomPart;
 }
 let shuffle = (array)=>{
     //fisher Yates algorithm
-    for(let i = 0; i<array.length-1; i--)
+    for(let i = array.length-1; i>0; i--)
     {
-        let randomNumber = Math.floor(Math.random()*(i+1))
-        [array[i].array[randomNumber]] = [array[randomNumber], array[i]];
+        let randomNumber = Math.floor(Math.random()*(i+1));
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
     }
+    console.log("Array shuffled");
 }
 
 
